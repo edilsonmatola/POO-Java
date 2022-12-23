e77935d27b3012e989534e2418890e70d5450416
Edilson Matola <edilsonmatola13@gmail.com>
2022-12-23 09:46:34 +0200

    Code refactor

--- algoritmos-Logica-Programacao/1000-algoritmos-Java/algorithms-1/atmProject/funcao.java
+++ algoritmos-Logica-Programacao/1000-algoritmos-Java/algorithms-1/atmProject/funcao.java
@@ -1,44 +1,31 @@
+class teste {
 
-import java.util.*;
-import java.io.*;
-import javax.swing.JOptionPane;
+    public String autenticar(String username, int senha, String nome[]) {
 
-class teste{
-
-    public static String autenticar(String username,int senha,String nome[]){
-        
-        for(int i=0;i<nome.length;i++){
-            if(username.equals(nome[i])){
+        for (String element : nome) {
+            if (username.equals(element)) {
                 return username;
-            }else{
+            } else {
                 return "erro";
             }
         }
 
         return "Error";
-      
+
     }
 
-    public static String [] banco(){
+    public String[] banco() {
 
-        String nome[] = {"Samuel","Sigia","Ends?"};
+        String nome[] = { "Samuel", "Sigia", "Ends?" };
         return nome;
     }
 }
 
+public class funcao {
 
-
-
-public class funcao{
-
-    public static void main(String[]args){
-    Scanner entrada= new Scanner(System.in);
-    
-
+    public static void main(String[] args) {
         teste f = new teste();
-        System.out.print(f.autenticar("Samuel",333,f.banco()));
+        System.out.print(f.autenticar("Samuel", 333, f.banco()));
 
-     
-
-}
+    }
 }
