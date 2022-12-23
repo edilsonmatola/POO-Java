1ca42cae4352891ff08187813fef2b770b4c7b4d
Edilson Matola <edilsonmatola13@gmail.com>
2022-12-23 09:47:24 +0200

    Code refactor

--- algoritmos-Logica-Programacao/1000-algoritmos-Java/algorithms-1/atmProject/testea.java
+++ algoritmos-Logica-Programacao/1000-algoritmos-Java/algorithms-1/atmProject/testea.java
@@ -1,50 +1,36 @@
+class teste {
 
-import java.util.*;
-import java.io.*;
-import javax.swing.JOptionPane;
+    public double autenticar(String username, int senha, String nome[][]) {
 
-class teste{
+        for (int i = 0; i < nome.length; i++) {
+            for (int o = 0; o < nome.length;) {
+                if (username.equals(nome[i][o])) {
+                    double senhaa = Double.parseDouble(nome[i][2]) - 100;
 
-    public static double autenticar(String username,int senha,String nome[][]){
-        
-        for(int i=0;i<nome.length;i++){
-            for (int o=0;o<nome.length;o++ ) {
-            	if(username.equals(nome[i][o])){
-            		double senhaa = Double.parseDouble(nome[i][2]) -100 ;
-
-                return senhaa;
-            }else{
-                return 1;
-            }
+                    return senhaa;
+                } else {
+                    return 1;
+                }
             }
         }
 
         return 2;
-      
+
     }
 
-    public static String [][] banco(){
+    public String[][] banco() {
 
-        String nome[][] = {{"Samuel","111","500"},{"Sigia","000","400"}};
+        String nome[][] = { { "Samuel", "111", "500" }, { "Sigia", "000", "400" } };
         return nome;
     }
 
-
 }
 
+public class testea {
 
-
-
-public class testea{
-
-    public static void main(String[]args){
-    Scanner entrada= new Scanner(System.in);
-    
-
+    public static void main(String[] args) {
         teste f = new teste();
-        System.out.print(f.autenticar("Samuel",333,f.banco()));
+        System.out.print(f.autenticar("Samuel", 333, f.banco()));
 
-     
-
-}
+    }
 }
