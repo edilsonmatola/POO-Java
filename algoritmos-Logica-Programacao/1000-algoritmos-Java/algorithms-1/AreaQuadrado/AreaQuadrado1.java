f510dde9eeb4019d399ee95227f3cc3b4a0caa46
Edilson Matola <edilsonmatola13@gmail.com>
2022-12-23 09:45:22 +0200

    Code refactor

--- algoritmos-Logica-Programacao/1000-algoritmos-Java/algorithms-1/AreaQuadrado/AreaQuadrado1.java
+++ algoritmos-Logica-Programacao/1000-algoritmos-Java/algorithms-1/AreaQuadrado/AreaQuadrado1.java
@@ -1,12 +1,15 @@
+import java.util.Scanner;
+
 import javax.swing.JOptionPane;
-import java.util.*;
-public class AreaQuadrado1{
-	public static void main(String[]args){
-		Scanner teclado=new Scanner(System.in);
-		System.out.println("Introduza O Valor Do Lado");
-		Double area=teclado.nextDouble();
-		double resultado=Math.pow(area,2);
-		System.out.println(resultado);
-		JOptionPane.showMessageDialog(null,"Volte Sempre!");
+
+public class AreaQuadrado1 {
+	public static void main(String[] args) {
+		try (Scanner teclado = new Scanner(System.in)) {
+			System.out.println("Introduza O Valor Do Lado");
+			Double area = teclado.nextDouble();
+			double resultado = Math.pow(area, 2);
+			System.out.println(resultado);
+		}
+		JOptionPane.showMessageDialog(null, "Volte Sempre!");
 	}
 }
