ce58204170c5b980062f0b68e57d3e73f7c5cff8
Edilson Matola <edilsonmatola13@gmail.com>
2022-12-23 09:40:15 +0200

    Code refactor

--- algoritmos-Logica-Programacao/1000-algoritmos-Java/algorithms-1/Altura Ideal/AlturaIdeal.java
+++ algoritmos-Logica-Programacao/1000-algoritmos-Java/algorithms-1/Altura Ideal/AlturaIdeal.java
@@ -1,19 +1,20 @@
+
 //Mack Banze
-import java.util.*;
-public class AlturaIdeal{
-	public static void main(String Args[]){
-		Scanner teclado=new Scanner(System.in);
-		System.out.println("Insira o seu peso");
-		String peso=teclado.nextLine();
-		String altura="";
-		//StringTokenizer token=new StringTokenizer(dados,".");
-		for (int i=0;i<peso.length() ;i++ ) {
-			if (i==0) {
-				
-			
-				altura=peso.substring(i,i+2);
+import java.util.Scanner;
+
+public class AlturaIdeal {
+	public static void main(String Args[]) {
+		try (Scanner teclado = new Scanner(System.in)) {
+			System.out.println("Insira o seu peso");
+			String peso = teclado.nextLine();
+			String altura = "";
+			// StringTokenizer token=new StringTokenizer(dados,".");
+			for (int i = 0; i < peso.length(); i++) {
+				if (i == 0) {
+					altura = peso.substring(i, i + 2);
+				}
+			}
+			System.out.println("A sua altura ideal e: 1." + altura + "metros");
 		}
-	}
-		System.out.println("A sua altura ideal e: 1."+altura+"metros");
 	}
 }
